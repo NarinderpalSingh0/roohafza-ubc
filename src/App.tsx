bd5415ce042d3de87c1318971bb4eef3ce7fd726
Narinderpal Singh <narinderpalsinghsidhu0@gmail.com>
2026-08-07 20:41:45 +0530

    Remove broken ambient sound button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { AppLayout } from "./components/layout/AppLayout";
 import { SectionRenderer } from "./components/layout/SectionRenderer";
 import Preloader from "./components/ui/Preloader";
 import ProgressIndicator from "./components/ui/ProgressIndicator";
-import AmbientSound from "./components/ui/AmbientSound";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -17,7 +16,6 @@ function App() {
     <AppProviders>
       {!loaded && <Preloader onComplete={handlePreloaderComplete} />}
       <ProgressIndicator />
-      <AmbientSound />
       <AppLayout>
         <SectionRenderer />
       </AppLayout>
